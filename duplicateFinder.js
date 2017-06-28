8bf77c8f7b33890b70f79ae53e2c09193f906142
Parami Gunasekara <parami@live.ca>
2017-06-28 17:40:09 -0400

    defined function structure

--- duplicateFinder.js
+++ duplicateFinder.js
@@ -3,5 +3,25 @@
  * finds possible duplicates and flags records for review.
  */
 function duplicateFinder() {
-    //TODO
+    //Variables for the database connection
+
+    //Variables for the data structure
+
+    //Variables for the current record
+
+    //Variables for the record comparison
+
+    //Variables for looping
+
+    //loop: Get current record
+
+    //loop through to find a duplicate
+        //if found
+            //get the row number from array position (+1)
+            //write the record on removed sheet (next empty)
+            //delete the row with the records (mind: when a row is deleted the counter should skip one)
+
+    //set next record as current record
+
+
 }
